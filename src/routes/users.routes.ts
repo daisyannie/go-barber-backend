beab399c52de2b07097e5438976691903efcd262
Daisy Annie <daisy.annie@shift.com.br>
2020-08-28 13:31:09 -0300

    Novas funcionalidades.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -10,18 +16,35 @@ usersRouter.post('/', async (request, response) => {
 
         const createUser = new CreateUserService();
 
-        const { user, token } = await createUser.execute({
-            name,
-            email,
-            password,
-        });
+        const user = await createUser.execute({ name, email, password });
 
         delete user.password;
 
-        return response.json({ user, token });
+        return response.json(user);
     } catch (err) {
         return response.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    },
+);
 
 export default usersRouter;
