f60b1832924b9c3ac06b2aa4883c19bfb4e5423b
Daisy Annie <daisy.annie@shift.com.br>
2020-10-13 07:37:08 -0300

    Novos ajustes.

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,3 +1,4 @@
+import { classToClass } from 'class-transformer';
 import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
@@ -43,7 +44,7 @@ class ListProviderAppointmentService {
                 },
             );
 
-            await this.cacheProvider.save(cacheKey, appointments);
+            await this.cacheProvider.save(cacheKey, classToClass(appointments));
         }
 
         return appointments;
