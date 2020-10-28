bd796d52a746dffeb5d449ff96192fcee6c65541
Daisy Annie <daisy.annie@shift.com.br>
2020-10-27 21:27:30 -0300

    .

--- src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -18,7 +18,7 @@ export default function ensureAuthenticated(
 ): void {
     const authHeader = request.headers.authorization;
     if (!authHeader) {
-        // throw new AppError('JWT token is missing', 401);
+        throw new AppError('JWT token is missing', 401);
     }
 
     // const [type, token] = authHeader.split(' ');
